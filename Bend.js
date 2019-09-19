3a3e923ac56f49fcfbbbcae2ce14e32c399a7bda
AreTea <tbusinessr@gmail.com>
2019-09-19 15:36:02 -0400

    adding nock

--- Bend.js
+++ Bend.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const nock = require('nock')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+const scope = nock('/')
+  .get('/')
+  .reply(200, "Hello World!")
 
 app.listen(port, () => console.log(`Simple Node/Express app listening on port ${port}!`))
