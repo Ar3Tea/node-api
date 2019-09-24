dc809cfb7f11b947555c038267b690c0b638904a
AreTea <tbusinessr@gmail.com>
2019-09-24 15:52:28 -0400

    adding Dockerfile

--- Bend.Test.js
+++ Bend.Test.js
@@ -1,29 +1,20 @@
+const express = require('express')
 const nock = require('nock');
 const request = require('supertest')("http://localhost");
 const expect = require('chai').expect;
+const app = express()
 
-describe("Testing API with a mocked backend", function () {
+describe("Testing API with a non-mocked backend", function () {
 
-  it("returns a successful mocked response", function (done) {
+  it("returns a successful response", function (done) {
     
-    //specify the url to be intercepted
-    nock("http://localhost")
-      //define the method to be intercepted
-      .get('/')
-      //respond with a OK and the specified JSON response
-      .reply(200, {
-        "status": 200,
-        "message": "Hello World!"
+    app.get('/', (req, res) => res.send('Hello World!'))
       });
     
-    //perform the request to the api which will now be intercepted by nock
     request
       .get('/')
-      .end(function (err, res) {
-        //assert that the mocked response is returned
-        expect(res.body.status).to.equal(200);
-        expect(res.body.message).to.equal("Hello World!");
+      .end(function (req, res) {
+        expect(res.body.message).to.equal('Hello World!');
         done();
       });
   })
-});
--- Bend.js
+++ Bend.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const nock = require('nock')
 const app = express()
 const port = 3000
 
