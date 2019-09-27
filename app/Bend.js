b1a548e347de203d969f9cee5c02ae901e5184a2
AreTea <tbusinessr@gmail.com>
2019-09-27 17:20:15 -0400

    testing systemd service

--- app/Bend.js
+++ app/Bend.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -11,3 +11,5 @@ app.get('/health', (req, res) => res.json({
 }))
 
 app.listen(port, () => console.log(`Simple Node/Express app listening on port ${port}!`))
+
+// testing systemd service
