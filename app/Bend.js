47b7f998cc436111dfb2cbb95343eb8214b561e2
AreTea <tbusinessr@gmail.com>
2019-09-28 12:02:49 -0400

    adding status to health check and gitignore

--- app/Bend.js
+++ app/Bend.js
@@ -1,14 +1,48 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Git = require("nodegit");
+ 
+// Open the repository directory.
+Git.Repository.open("tmp")
+  // Open the master branch.
+  .then(function(repo) {
+    return repo.getMasterCommit();
+  })
+  // Display information about commits on master.
+  .then(function(firstCommitOnMaster) {
+    // Create a new history event emitter.
+    let history = firstCommitOnMaster.history();
+ 
+    // Create a counter to only show up to 9 entries.
+    let count = 0;
+ 
+    // Listen for commit events from the history.
+    history.on("commit", function(commit) {
+      // Disregard commits past 3.
+      if (++count >= 3) {
+        return;
+      }
+
+      let author = commit.author();
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/health', (req, res) => res.json({ 
     status: 'OK',
-    version: 'commit here',
-    commitMessage: 'message maybe here'
+    commit: commit.sha(),
+    date: "Date:\t" + commit.date(),
+    author: author.name() + " <" + author.email() + ">",
+    commitMessage: commit.message()
 }))
+
+});
+ 
+// Start emitting events.
+history.start();
+});
+
 
 app.listen(port, () => console.log(`Simple Node/Express app listening on port ${port}!`))
 
